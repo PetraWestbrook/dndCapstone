1948b4cb841f13fb72d8373fd940e19a53577fb2
Petra Westbrook <westbrop@wwt.com>
2022-02-06 09:39:47 -0600

    master - Clean up auto generated code base

--- web/cypress/integration/common/app_step_def.js
+++ web/cypress/integration/common/app_step_def.js
@@ -5,13 +5,3 @@ import {
 Given('I am on the app', () => {
   cy.visit('/');
 });
-
-Given('I am on the Project Page', () => {
-  cy.visit('/projects');
-});
-
-Then('I see {string}', (label) => {
-  cy.get('body')
-    .contains(label)
-    .should('exist');
-});
