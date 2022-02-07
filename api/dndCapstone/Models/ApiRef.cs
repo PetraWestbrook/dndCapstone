ba64f77aeddb32db993a4212d3abe0f4ba8464e1
Petra Westbrook <westbrop@wwt.com>
2022-02-07 13:56:29 -0600

    master - add and populate common reference model

---
+++ api/dndCapstone/Models/ApiRef.cs
@@ -0,0 +1,9 @@
+namespace APIReference
+{
+    public class commonModel
+    {
+        public string index { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+    }
+}
