7910edc040b7a2ab3c6f22c35f4c0d8b3823dfb2
Petra Westbrook <westbrop@wwt.com>
2022-02-09 12:37:45 -0600

    master - add Proficiencies model

--- api/dndCapstone/Models/Monsters.cs
+++ api/dndCapstone/Models/Monsters.cs
@@ -1,14 +1,9 @@
 using System;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
 
 namespace MonsterApiConnection
 {
     public class Monsters
     {
-        public string index { get; set; }
         public string name { get; set; }
         public string size { get; set; }
         public string type { get; set; }
@@ -17,35 +12,40 @@ namespace MonsterApiConnection
         public int armor_class { get; set; }
         public int hit_points { get; set; }
         public string hit_dice { get; set; }
-    }
-    class MonsterCall
+        public commonModel.APIRef[] forms { get; set; }
+        public object speed { get; set; }
+        public int strength { get; set; }
+        public int dexterity { get; set; }
+        public int constitution { get; set; }
+        public int intelligence { get; set; }
+        public int wisdom { get; set; }
+        public int charisma { get; set; }
+        public proficiencyModel.proficienciesClass[] proficiencies {get;set;}
+}
+
+class MonsterCall
+{
+    static HttpClient client = new HttpClient();
+    static string baseAddress = "https://www.dnd5eapi.co/api/";
+    static async Task<Uri> CreateMonsterAsync(Monsters monster)
     {
-        static HttpClient client = new HttpClient();
-        static string baseAddress = "https://www.dnd5eapi.co/api/";
-        static void ShowMonster(Monsters monster)
-        {
-            Console.WriteLine($"name: {monster.name}");
-        }
+        HttpResponseMessage response = await client.PostAsJsonAsync(
+            $"{baseAddress}monsters/aboleth", monster);
+        response.EnsureSuccessStatusCode();
 
-        static async Task<Uri> CreateMonsterAsync(Monsters monster)
-        {
-            HttpResponseMessage response = await client.PostAsJsonAsync(
-                $"{baseAddress}monsters/aboleth", monster);
-            response.EnsureSuccessStatusCode();
-
-            // return URI of the created resource.
-            return response.Headers.Location;
-        }
-
-        public static async Task<Monsters> GetMonstersAsync(string path)
-        {
-            Monsters monster = null;
-            HttpResponseMessage response = await client.GetAsync($"{baseAddress}monsters/{path}");
-            if (response.IsSuccessStatusCode)
-            {
-                monster = await response.Content.ReadFromJsonAsync<Monsters>();
-            }
-            return monster;
-        }
+        // return URI of the created resource.
+        return response.Headers.Location;
     }
+
+    public static async Task<Monsters> GetMonstersAsync(string path)
+    {
+        Monsters monster = null;
+        HttpResponseMessage response = await client.GetAsync($"{baseAddress}monsters/{path}");
+        if (response.IsSuccessStatusCode)
+        {
+            monster = await response.Content.ReadFromJsonAsync<Monsters>();
+        }
+        return monster;
+    }
+}
 }
---
+++ api/dndCapstone/Models/Proficiencies.cs
@@ -0,0 +1,9 @@
+namespace proficiencyModel
+{
+    public class proficienciesClass
+    {
+        public commonModel.APIRef proficiency { get; set; }
+        public int value { get; set; }
+    }
+}
+
