49808a5a9f9b81a7da0d3967c9b43b749e56e6dc
Petra Westbrook <westbrop@wwt.com>
2022-02-20 14:02:41 -0600

    master - add DC model

---
+++ api/dndCapstone/Models/DC.cs
@@ -0,0 +1,9 @@
+namespace DCModel
+{
+    public class DCClass
+    {
+        public commonModel.APIRef dc_type { get; set; }
+        public int dc_value { get; set; }
+        public string success_type { get; set; }
+    }
+}
