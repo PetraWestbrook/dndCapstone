ee483bd8eece97f6a87e3e2448262e47ee6928d7
Petra Westbrook <westbrop@wwt.com>
2022-02-20 16:51:26 -0600

    master - add From and Options model

---
+++ api/dndCapstone/Models/From.cs
@@ -0,0 +1,9 @@
+namespace FromModel
+{
+    public class FromClass
+    {
+        public string name { get; set; }
+        public int count { get; set; }
+        public string type { get; set; }
+    }
+}
---
+++ api/dndCapstone/Models/Options.cs
@@ -0,0 +1,8 @@
+namespace OptionsModel
+{
+    public class OptionsClass
+    {
+        public int choose { get; set; }
+        public FromModel.FromClass[][] from { get; set; }
+    }
+}
