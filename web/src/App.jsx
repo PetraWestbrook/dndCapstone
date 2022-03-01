938efda20f86c3cc72f08dd2b4fb8a3f31084380
Petra Westbrook <westbrop@wwt.com>
2022-02-28 22:58:48 -0600

    master - change name of MonsterList component to RandomMonster

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -4,7 +4,7 @@ import './App.scss';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
 import Profile from './components/Profile';
-import Monsters from './components/MonsterList';
+import Monsters from './components/RandomMonster';
 
 function App () {
   const { isLoading } = useAuth0();
